73a1cce8d768263b7b8c7981d7baf60cfce3d0d7
Jake Adler <63482878+jrjradler@users.noreply.github.com>
2021-11-17 16:09:14 -0500

    network swap

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -184,6 +184,10 @@ function SafeList({ provider }) {
         setSafeTxInfoAll(null);
         console.error(err);
       }
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x1' }], // chainId must be in hexadecimal numbers
+      });
     }
     fetchAccount();
   }, [account, provider, setSafeTxInfo, setSafeTxInfoAll]);
